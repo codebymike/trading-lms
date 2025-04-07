10db270e303596b4a43522a9a6edf3dc44ac0c8f
Mike Thomas <mikethomas84@gmail.com>
2025-04-07 13:27:31 +0100

    validation messages

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -1,8 +1,8 @@
 import {z} from 'zod';
 
 export const authSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8),
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z.string().min(8, { message: 'Password must be at least 8 characters long' }),
 });
 
 export const loginSchema = authSchema
