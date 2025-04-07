d43164fc8aab52d4b41bc15f4949ac2f5e4908b3
Mike Thomas <mikethomas84@gmail.com>
2025-04-07 13:05:24 +0100

    register schema

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -5,3 +5,10 @@ export const authSchema = z.object({
   password: z.string().min(8),
 });
 
+export const loginSchema = authSchema
+export const registerSchema = authSchema.extend({
+    name: z.string().min(1).max(255).default('')
+})
+
+export type loginSchema = z.output<typeof loginSchema>;
+export type registerSchema = z.output<typeof registerSchema>;
