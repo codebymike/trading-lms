ffde5441c3fd19cde745787762cef6193c6a3ba9
Mike Thomas <mikethomas84@gmail.com>
2025-04-08 12:22:22 +0100

    check user is unique

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -3,4 +3,18 @@ import db from "~/lib/prisma";
 
 export default defineEventHandler(async (event) => {
     const { name, password, email } = await readValidatedBody(event, (body) => registerSchema.parse(body));
+
+    const userAlreadyExists = await db.user.findUnique({
+        where: {
+            email
+        }
+    });
+
+    if( userAlreadyExists ) {
+        throw createError({
+            statusCode: 409,
+            statusMessage: 'User already exists'
+        });
+    }
+
 });
