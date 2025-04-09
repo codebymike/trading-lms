99749f051c3a3d98444b4e6a933ca6a156dae56b
Mike Thomas <mikethomas84@gmail.com>
2025-04-09 23:41:52 +0100

    toggle + set states

--- composables/useStore.ts
+++ composables/useStore.ts
@@ -7,5 +7,15 @@ const state = reactive<State>({
 })
 
 export default () => {
+    const { isLoading, apiError, isConfirmModalVisible } = toRefs(state);
     
+    const toggleLoading = (value: boolean) => {
+        state.isLoading = value;
+    }
+    const toggleConfirmModal = (value: boolean) => {
+        state.isConfirmModalVisible = value;
+    }
+    const setApiError = (error: APIError | null) => {
+        state.apiError = error;
+    }
 }
