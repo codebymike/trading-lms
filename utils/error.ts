f09d888b436d2d73ade258d59eb452faabc9614b
Mike Thomas <mikethomas84@gmail.com>
2025-04-10 23:20:25 +0100

    handleError params

---
+++ utils/error.ts
@@ -0,0 +1,7 @@
+export const handleError = (error: any) => {
+    const generalMessage = "An unexpected error occurred"
+    let statusCode = 500
+    let statusMessage = "Internal Server Error"
+    let message = generalMessage
+    let data = undefined
+}
