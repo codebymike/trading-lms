84a20d05199e8055be2dc8433019d1d9ced23909
Mike Thomas <mikethomas84@gmail.com>
2025-04-12 17:45:22 +0100

    error data + message

--- utils/error.ts
+++ utils/error.ts
@@ -4,4 +4,15 @@ export const handleError = (error: any) => {
     let statusMessage = "Internal Server Error"
     let message = generalMessage
     let data = undefined
+
+    if ( error.response ) {
+        statusCode = error.response.status
+        statusMessage = error.response.statusText
+
+        if ( error.response._data ) {
+            data = error.response._data && error.response.errors ? error.response.errors : undefined
+            message = error.response._data && error.response._data.message ? error.response._data.message : generalMessage
+        }
+
+    }
 }
