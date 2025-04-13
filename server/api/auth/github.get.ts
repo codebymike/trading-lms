9f573aab3fa43ac12204c2278c20bf9bea41774e
Mike Thomas <mikethomas84@gmail.com>
2025-04-13 19:21:59 +0100

    find or create github user

--- server/api/auth/github.get.ts
+++ server/api/auth/github.get.ts
@@ -1,8 +1,28 @@
+import db from "~/lib/prisma";
+import { santiseUser } from "~/utils/auth";
+
 export default defineOAuthGitHubEventHandler({
     config: {
         emailRequired: true,
     },
-    async onSuccess( event, { user, tokens } ) {
+    async onSuccess( event, { user } ) {
+
+        let currentUser = await db.user.findUnique({
+            where: {
+                email: user.email
+            }
+        })
+
+        if( !currentUser ) {
+            currentUser = await db.user.create({
+                data: {
+                    email: user.email,
+                    name: user.name,
+                    avatarUrl: user.avatar_url,
+                }
+            })
+        }
+
         await setUserSession( event, {
             user: {
                 githubId: user.id,
