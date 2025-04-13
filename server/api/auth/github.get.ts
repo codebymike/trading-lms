ec659c60bd7923748c2f240eb908813297cea365
Mike Thomas <mikethomas84@gmail.com>
2025-04-13 19:14:25 +0100

    event handler on success

--- server/api/auth/github.get.ts
+++ server/api/auth/github.get.ts
@@ -1,3 +1,13 @@
 export default defineOAuthGitHubEventHandler({
-    
+    config: {
+        emailRequired: true,
+    },
+    async onSuccess( event, { user, tokens } ) {
+        await setUserSession( event, {
+            user: {
+                githubId: user.id,
+            }
+        })
+        return sendRedirect( event, '/' )
+    }
 })
