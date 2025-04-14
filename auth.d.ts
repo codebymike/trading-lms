4a9d1628ee522573dc4612c239d7ddf0062333de
Mike Thomas <mikethomas84@gmail.com>
2025-04-14 17:20:44 +0100

    auth types

--- auth.d.ts
+++ auth.d.ts
@@ -1,3 +1,20 @@
 declare module '#auth-utils' {
-
-}
+    interface User {
+        id: string
+        email: string
+        name: string | null
+        avatarUrl: string | null
+        createdAt: Date
+        updatedAt: Date
+    }
+  
+    interface UserSession {
+      // Add your own fields
+    }
+  
+    interface SecureSessionData {
+      // Add your own fields
+    }
+  }
+  
+  export {}
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <pre>
-            {{ user }}
-        </pre>
+        <pre>{{ user }}</pre>
         <UButton @click=logout>Logout</UButton>
     </div>
 </template>
 <script setup lang="ts">
+import { navigateTo } from 'nuxt/app'
+
 const {user, clear} = useUserSession()
 
 const logout = async () => {
