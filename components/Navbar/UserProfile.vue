f1dca5da7fc86568ba4865556473b228b4080a7c
Mike Thomas <mikethomas84@gmail.com>
2025-04-16 15:46:14 +0100

    userprofile i

---
+++ components/Navbar/UserProfile.vue
@@ -0,0 +1,18 @@
+<template>
+    <UDropdown :items="items" :ui="{ item: { disabled: 'cursor-text select-text' } }" :popper="{ placement: 'bottom-start' }">
+        <UAvatar icon="lucide:circle-user" size="sm" color="black" />
+    </UDropdown>
+</template>
+
+<script setup lang="ts">
+const items = [
+    [{
+        label:'',
+        slot: 'account',
+        disabled: true
+    }],[{
+        label: 'Settings',
+        icon: 'lucide:settings',
+    }]
+]
+</script>
