a6988ddc201516afbdd1e298352e5898952870bc
Mike Thomas <mikethomas84@gmail.com>
2025-04-16 15:18:55 +0100

    navroutes template

--- components/Navbar/Routes.vue
+++ components/Navbar/Routes.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-
+        <div class="hidden md:block" v-if="isSearchPage">
+            <NavbarInput></NavbarInput>
+        </div>
+        <div class="flex gap-x-2 ml-auto">
+            <UButton v-if="isTeacherPage || isCoursePage" to="/" label="Exit" icon="lucid:logout" size="sm" variant="ghost"></UButton>
+            <UButton v-else to="/teacher" label="Teacher Mode" size="sm" variant="ghost"></UButton>
+        </div>
     </div>
 </template>
 
