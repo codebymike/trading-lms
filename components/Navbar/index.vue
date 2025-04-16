c65493e3b9f6993f44da84568c719863c6c823c4
Mike Thomas <mikethomas84@gmail.com>
2025-04-16 15:08:53 +0100

    navbar routes

---
+++ components/Navbar/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="p-4 border-b h-full flex items-center bg-white shadow-sm">
+        <NavbarRoutes></NavbarRoutes>
+    </div>
+</template>
+
+<script lang="ts">
+
+</script>
