858af48ab4982fe3eb50c565d661e2be750e2698
Mike Thomas <mikethomas84@gmail.com>
2025-04-16 15:13:33 +0100

    computed routes

---
+++ components/Navbar/Routes.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+const isTeacherPage = computed(() => {
+    return route.path.startsWith('/teacher')
+})
+const isCoursePage = computed(() => {
+    return route.path.startsWith('/courses')
+})
+const isSearchPage = computed(() => {
+    return route.path.startsWith('/search')
+})
+
+</script>
