41dfb23f2a2eaa5d0019ab0d98656fa1ecc458d9
Mike Thomas <mikethomas84@gmail.com>
2025-04-16 15:54:00 +0100

    profile logout

--- components/Navbar/UserProfile.vue
+++ components/Navbar/UserProfile.vue
@@ -2,7 +2,7 @@
     <UDropdown :items="items" :ui="{ item: { disabled: 'cursor-text select-text' } }" :popper="{ placement: 'bottom-start' }">
         <UAvatar icon="lucide:circle-user" size="sm" color="black" />
 
-        <template #account="{ item }">
+        <template #account="">
             <div class="text-left">
                 <p>
                     Signed in as
@@ -14,15 +14,22 @@
         </template>
 
         <template #item="{ item }">
-            <span class="truncate">{{ item.label }}</span>
-            <UIcon :name="item.icon" class="flex shrink-0 h-4 w-4 text-gray-400 dark:text-gray-500 ms-auto" />
+            <div @click="logout" class="flex justify-between items-center w-full">
+                <span class="truncate">{{ item.label }}</span>
+                <UIcon :name="item.icon" class="flex shrink-0 h-4 w-4 text-gray-400 dark:text-gray-500 ms-auto" />
+            </div>
         </template>
 
     </UDropdown>
 </template>
 
 <script setup lang="ts">
-const { user } = useUserSession()
+const { user, clear } = useUserSession()
+
+const logout = async () => {
+    await clear()
+    await navigateTo('/auth')
+}
 
 const items = [
     [{
