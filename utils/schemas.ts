1632597e51717fc7e9a800c3957ae35d557d177d
Mike Thomas <mikethomas84@gmail.com>
2025-04-17 14:18:37 +0100

    course schema + type

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -1,5 +1,7 @@
+import { title } from 'process';
 import {z} from 'zod';
 
+// Auth
 export const authSchema = z.object({
   email: z.string().email({ message: 'Invalid email address' }),
   password: z.string().min(8, { message: 'Password must be at least 8 characters long' }),
@@ -12,3 +14,10 @@ export const registerSchema = authSchema.extend({
 
 export type LoginSchema = z.output<typeof loginSchema>;
 export type RegisterSchema = z.output<typeof registerSchema>;
+
+
+// Course
+export const courseSchema = z.object({
+  title: z.string().min(1, { message: 'Title is required' }),
+});
+export type CourseSchema = z.output<typeof courseSchema>;
