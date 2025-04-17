bb33ea41234b30f9bc007ccc8a559eec85d21e39
Mike Thomas <mikethomas84@gmail.com>
2025-04-17 14:57:36 +0100

    teacher courses post api basic

---
+++ server/api/teacher/courses/index.post.ts
@@ -0,0 +1,18 @@
+import db from "~/lib/prisma"
+import { courseSchema } from "~/utils/schemas"
+
+export default defineEventHandler(async (event) => {
+    await requireUserSession(event)
+    const session = await getUserSession(event)
+
+    if( session.user ) {
+        const { title } = await readValidatedBody(event, ( body ) => courseSchema.parse(body))
+
+        await db.course.create({
+            data: {
+                title,
+                userId: session.user.id
+            }
+        })
+    }
+})
