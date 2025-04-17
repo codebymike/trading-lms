544b7fc2dd3227720066d0f3abf350d6eed26f18
Mike Thomas <mikethomas84@gmail.com>
2025-04-17 14:26:57 +0100

    form basic style

--- pages/teacher/courses/create/index.vue
+++ pages/teacher/courses/create/index.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
         <div>
-            <h1>Name your course</h1>
-            <p>What would you like to name your course, don't worry you can change this later</p>
+            <h1 class="text-2xl">Name your course</h1>
+            <p class="text-sm text-slate-600">What would you like to name your course, don't worry you can change this later</p>
             <UForm :schema="courseSchema" :state="courseForm" @submit="onSubmit">
                 <div class="space-y-8 mt-8">
                     <UFormField label="Course title" name="title" help="What will you teach in this course?">
-                        <UInput v-model="courseForm.title" placeholder="Vue.js Development" />
+                        <UInput v-model="courseForm.title" placeholder="Vue.js Development" class="w-full"/>
                     </UFormField>
                     <div class="flex items-center gap-x-2">
                         <UButton to="/" variant="soft">Cancel</UButton>
