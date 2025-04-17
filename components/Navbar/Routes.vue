ce598111d62eaffabbba748015b8d7a3f940a66c
Mike Thomas <mikethomas84@gmail.com>
2025-04-17 14:08:24 +0100

    fixed navroute layout

--- components/Navbar/Routes.vue
+++ components/Navbar/Routes.vue
@@ -1,13 +1,11 @@
 <template>
-    <div>
-        <div class="hidden md:block" v-if="isSearchPage">
-            <NavbarInput></NavbarInput>
-        </div>
-        <div class="flex gap-x-2 ml-auto">
-            <UButton v-if="isTeacherPage || isCoursePage" to="/" label="Exit" icon="lucide:log-out" size="sm" variant="ghost"></UButton>
-            <UButton v-else to="/teacher" label="Teacher Mode" size="sm" variant="ghost"></UButton>
-            <NavbarUserProfile />
-        </div>
+    <div class="hidden md:block" v-if="isSearchPage">
+        <NavbarInput></NavbarInput>
+    </div>
+    <div class="flex gap-x-2 ml-auto">
+        <UButton v-if="isTeacherPage || isCoursePage" to="/" label="Exit" icon="lucide:log-out" size="sm" variant="ghost"></UButton>
+        <UButton v-else to="/teacher" label="Teacher Mode" size="sm" variant="ghost"></UButton>
+        <NavbarUserProfile />
     </div>
 </template>
 
