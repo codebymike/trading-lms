0333fa340427a3b888b9eb75a12c8608c07ff365
Mike Thomas <mikethomas84@gmail.com>
2025-04-18 23:44:51 +0100

    fetch and list courses

--- pages/teacher/courses/index.vue
+++ pages/teacher/courses/index.vue
@@ -2,12 +2,19 @@
     <div>
         Teacher Courses
     </div>
+    <div v-if="status === 'pending' && courses">
+        <NuxtLink class="mb-1" v-for="course in courses" :key="course.id" :to="'/teacher/courses/${course.id}'">{{ course.title }}</NuxtLink>
+    </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
     layout: 'teacher',
     middleware: 'protected',
+})
+
+const { data : courses, status } = await useFetch('/api/teacher/courses', {
+    method: 'GET',
 })
 
 
