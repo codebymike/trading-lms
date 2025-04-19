92dae798cbbf07d9b9b08a1b4c3fe88edef9ea07
Mike Thomas <mikethomas84@gmail.com>
2025-04-19 20:30:34 +0100

    unpub alert

--- pages/teacher/courses/[courseId]/index.vue
+++ pages/teacher/courses/[courseId]/index.vue
@@ -1,7 +1,18 @@
 <template>
-    <div>
-        <pre>{{ course }}</pre>
+    <UAlert 
+        v-if="status !== 'pending' && course && !course.isPublished" 
+        icon="i-lucide-alert-triangle" 
+        title="This course is unpublished. It will not be visible to students." 
+        color="warning" 
+        variant="soft">
+    </UAlert>
+
+    <div class="p-6">
+        <div class="flex items-center justify-between">
+            
+        </div>
     </div>
+
 </template>
 
 <script setup lang="ts">
@@ -13,8 +24,8 @@ definePageMeta({
 
 const { params } = useRoute()
 
-const { data : course } = await useFetch(`/api/teacher/courses/${params.courseId}`, {
-    key: `Course-${params.courseId}`,
+const { data : course, status } = await useFetch(`/api/teacher/courses/${params.courseId}`, {
+    key: `Teacher-Course-${params.courseId}`,
 })
 
 </script>
