a5b8e56807f889b5af40a00653076de59368175f
Mike Thomas <mikethomas84@gmail.com>
2025-04-19 20:11:18 +0100

    course display working

--- pages/teacher/courses/index.vue
+++ pages/teacher/courses/index.vue
@@ -2,12 +2,13 @@
     <div>
         Teacher Courses
     </div>
-    <div v-if="status === 'pending' && courses">
-        <NuxtLink class="mb-1" v-for="course in courses" :key="course.id" :to="'/teacher/courses/${course.id}'">{{ course.title }}</NuxtLink>
+    <div v-if="status !== 'pending' && courses">
+        <NuxtLink class="mb-1 text-primary-50" v-for="course in courses" :key="course.id" :to="'/teacher/courses/${course.id}'">{{ course.title }}</NuxtLink>
     </div>
 </template>
 
 <script setup lang="ts">
+
 definePageMeta({
     layout: 'teacher',
     middleware: 'protected',
@@ -16,6 +17,4 @@ definePageMeta({
 const { data : courses, status } = await useFetch('/api/teacher/courses', {
     method: 'GET',
 })
-
-
 </script>
