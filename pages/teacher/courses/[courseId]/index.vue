26a0aaf3babf0ded7b7178fe17fa8c99516c8f0c
Mike Thomas <mikethomas84@gmail.com>
2025-04-19 20:35:51 +0100

    required fields comps

--- pages/teacher/courses/[courseId]/index.vue
+++ pages/teacher/courses/[courseId]/index.vue
@@ -9,7 +9,7 @@
 
     <div class="p-6">
         <div class="flex items-center justify-between">
-            
+
         </div>
     </div>
 
@@ -27,5 +27,19 @@ const { params } = useRoute()
 const { data : course, status } = await useFetch(`/api/teacher/courses/${params.courseId}`, {
     key: `Teacher-Course-${params.courseId}`,
 })
+
+const requiredFields = computed( () => {
+    return [
+        course.value?.title,
+        course.value?.description,
+        course.value?.imageUrl,
+        course.value?.price,
+        course.value?.categoryId,
+        course.value?.chapters.some( chapter => chapter.isPublished )
+    ]
+})
+
+const totalFields = computed( () => requiredFields.value.length )
+const completedFields = computed( () => requiredFields.value.filter(Boolean).length )
 
 </script>
