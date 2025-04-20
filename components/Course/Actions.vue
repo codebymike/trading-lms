214751bc88b03497a152b11d2b34f3be3aaa9cae
Mike Thomas <mikethomas84@gmail.com>
2025-04-20 22:22:39 +0100

    course/actions comp

---
+++ components/Course/Actions.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex items-center gap-x-2">
+        <UButtom color="gray" :label="courseSchema.isPublished ? 'Unpublish' : 'Publish'" @click="toggleCourse" :disabled="isLoading"></UButtom>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { Course } from '@prisma/client'
+
+</script>
