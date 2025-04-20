ec56057d1b4b1556c61b8699b73b8c96f4efa640
Mike Thomas <mikethomas84@gmail.com>
2025-04-20 22:24:47 +0100

    actions store + toggle

--- components/Course/Actions.vue
+++ components/Course/Actions.vue
@@ -7,4 +7,14 @@
 <script setup lang="ts">
 import type { Course } from '@prisma/client'
 
+const props = defineProps<{
+    course: Course
+}>()
+
+const { isLoading, toggleConfirmModal } = useStore()
+
+const toggleCourse = () => {
+    props.course.isPublished = !props.course.isPublished
+}
+
 </script>
