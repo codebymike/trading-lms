b43a0c62f2029cf0c926e585d071840eaf83e963
Mike Thomas <mikethomas84@gmail.com>
2025-04-20 22:34:51 +0100

    modal template

--- components/ConfirmModal.vue
+++ components/ConfirmModal.vue
@@ -1,9 +1,22 @@
 <template>
     <div>
+        <UButton label="Open" @click="isOpen = true" />
+        <UModal :open="isOpen">
+            <UCard>
+                <template #header>
+                    <Placeholder class="h-8" />
+                </template>
 
+                <Placeholder class="h-32" />
+
+                <template #footer>
+                    <Placeholder class="h-8" />
+                </template>
+            </UCard>
+        </UModal>
     </div>
 </template>
 
 <script setup lang="ts">
-
+const isOpen = ref(false)
 </script>
