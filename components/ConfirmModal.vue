83a0eed054402a4f890204ffac369a4bcb959197
Mike Thomas <mikethomas84@gmail.com>
2025-04-21 15:15:30 +0100

    modal content working

--- components/ConfirmModal.vue
+++ components/ConfirmModal.vue
@@ -1,27 +1,26 @@
 <template>
     <div>
         <UModal :open="isOpen" prevent-close>
-            <UCard :ui="{ root: 'h-full flex flex-col' }">
-                
-                <div class="space-y-3">
-                    <h3 class="text-base font-semibold leading-6 text-gray-900">Are you sure?</h3>
-                    <p>This action cannot be undone.</p>
-                </div>
-
-                <template #footer>
-                    <div class="flex w-full justify-end gap-x-2 items-center">
-                        <UButton color="info" variant="ghost" @click="toggleConfirmModal(false)" label="Cancel" />
-                        <UButton color="primary" variant="soft" @click="emit('onConfirm')" label="Continue" />
+            <template #content>
+                <UCard>
+                    <div class="space-y-3">
+                        <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-gray-200">Are you sure?</h3>
+                        <p>This action cannot be undone.</p>
                     </div>
-                </template>
-            </UCard>
+
+                    <template #footer>
+                        <div class="flex w-full justify-end gap-x-2 items-center">
+                            <UButton color="info" variant="ghost" @click="toggleConfirmModal(false)" label="Cancel" />
+                            <UButton color="primary" variant="soft" @click="emit('onConfirm')" label="Continue" />
+                        </div>
+                    </template>
+                </UCard>
+            </template>
         </UModal>
     </div>
 </template>
 
 <script setup lang="ts">
-import { emit } from 'process'
-
 const { isConfirmModalVisible, toggleConfirmModal } = useStore()
 
 const isOpen = computed({
