4eb499f9eb89ea9bc4a59698b63e9d55d2b4e6be
Mike Thomas <mikethomas84@gmail.com>
2025-04-23 16:09:56 +0100

    update title patch

--- server/api/teacher/courses/[courseId]/index.patch.ts
+++ server/api/teacher/courses/[courseId]/index.patch.ts
@@ -4,7 +4,18 @@ export default defineEventHandler(async (event) => {
     await requireUserSession(event)
     const session = await getUserSession(event)
 
+    // TODO add schema validation
+    const body = await readBody(event)
+
     if( session.user ) {
-      
-    }
+        return await db.course.update({
+            where: {
+                id: event.context.params?.courseId,
+                userId: session.user.id
+            },
+            data: {
+                title: body.title,
+            }
+        })
+    }   
 })
