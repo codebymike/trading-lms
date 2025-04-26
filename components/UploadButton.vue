985af6bb233e1bffd82d4284794095d4da80e7dd
Mike Thomas <mikethomas84@gmail.com>
2025-04-26 17:44:34 +0100

    correct types for Result I

--- components/UploadButton.vue
+++ components/UploadButton.vue
@@ -1,9 +1,12 @@
 <template>
-    <CldUploadWidget v-slot="{ open }" uploadPreset="nuxt-cloudinary-unsigned" :options="{ clientAllowedFormats: allowedFormats, resourceType: undefined}" :onUpload="handleUpload">
-        <UCard @click="open" class="cursor-pointer h-36 flex justify-center items-center" :ui="{ 
-            shadow: 'shadow-none', 
-            background: 'bg-transparent',
-            rounded: 'rounded-none'}">
+    <CldUploadWidget 
+        v-slot="{ open }" 
+        uploadPreset="nuxt-cloudinary-unsigned" 
+        :options="{ clientAllowedFormats: allowedFormats, resourceType: undefined}" 
+        :onResult="handleUpload" 
+        :onError="handleError"
+        >
+        <UCard @click="open" class="cursor-pointer h-36 flex justify-center items-center">
             <div class="flex flex-col justify-center items-center w-full">
                 <Icon name="lucide:upload" class="mb-2 size-6" />
                 <div class="text-xs text-muted-foreground mt-4 text-center">
@@ -28,17 +31,30 @@ defineProps<UploadButtonProps>()
 const emit = defineEmits(['onChange'])
 
 interface Result {
-    info: {
-        secure_url: string
+    info?: {
+        secure_url?: string
+    } | string
+}
+
+const handleUpload = (result: Result) => {
+
+    console.log('Upload result:', result)
+    const secureUrl = typeof result.info === 'object' && result.info?.secure_url 
+        ? result.info.secure_url 
+        : typeof result.info === 'string' 
+        ? result.info 
+        : undefined;
+
+    if (secureUrl) {
+        console.log('Secure URL:', secureUrl);
+        emit('onChange', secureUrl);
+    } else {
+        console.error('Secure URL not found in result:', result);
     }
 }
 
-const handleUpload = (result: Ref<Result>) => {
-
-    console.log('Upload result:', result.value)
-    console.log(result.value.info.secure_url)
-
-    emit('onChange', result.value.info.secure_url)
+const handleError = (error: any) => {
+    console.error('Upload error:', error)
 }
 
 </script>
