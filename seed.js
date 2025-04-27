ff78050c22cfaf5189c46ab1c3a9a6e8a8f4c221
Mike Thomas <mikethomas84@gmail.com>
2025-04-27 21:42:12 +0100

    seed category list

---
+++ seed.js
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+async function createCategories() {
+  try {
+    const categories = [
+      { name: "Finance" },
+      { name: "Blockchain" },
+      { name: "Accounting" },
+      { name: "Taxes" },
+      { name: "Other" },
+    ];
+
+    for (const category of categories) {
+      await db.category.create({ data: category });
+    }
+  } catch (error) {
+    console.error("Error creating categories:", error);
+  }
+}
